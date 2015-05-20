b2a808c71f0795731ed952c96c8eea1bf7f73214
RobertBerger5 <robertberger5@gmail.com>
2015-05-20 14:50:02 -0500

    Update teamwork.js

--- teamwork.js
+++ teamwork.js
@@ -72,16 +72,16 @@ var reset = function () {
 
 // Update game objects
 var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
+	if (87 in keysDown) { // Player holding up
 		hero.y -= hero.speed * modifier;
 	}
-	if (40 in keysDown) { // Player holding down
+	if (83 in keysDown) { // Player holding down
 		hero.y += hero.speed * modifier;
 	}
-	if (37 in keysDown) { // Player holding left
+	if (65 in keysDown) { // Player holding left
 		hero.x -= hero.speed * modifier;
 	}
-	if (39 in keysDown) { // Player holding right
+	if (68 in keysDown) { // Player holding right
 		hero.x += hero.speed * modifier;
 	}
 	
