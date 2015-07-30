6ba08d39656fe152334bec8ab246b4ec90307fc0
RobertBerger5 <robertberger5@gmail.com>
2015-07-30 03:08:58 -0500

    Create doggy.js

---
+++ sexgame/doggy.js
@@ -0,0 +1,18 @@
+document.getElementById("out").src="http://i.imgur.com/iCMc1in.png";
+document.getElementById("in").src="http://i.imgur.com/G7s8YfL.png";
+document.getElementById("jizz").src="http://i.imgur.com/pnLXJqU.png";
+var out = true;
+var creamed=false;
+setInterval(function(){
+	if(creamed){
+		document.getElementById("jizz").style.opacity=1;
+	}else{
+		if(out){
+			document.getElementById("in").style.opacity=1;
+			out=false;
+		}else{
+			document.getElementById("in").style.opacity=0;
+			out=true;
+		};
+	};
+},100);
