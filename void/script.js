53ab1f0bcd23966e864a6474af0322ba4870881c
RobertBerger5 <robertberger5@gmail.com>
2016-05-20 18:16:07 -0500

    Update script.js

--- void/script.js
+++ void/script.js
@@ -1,3 +1,36 @@
-function redirect(href){
-  window.location.href = "http://robertberger5.github.io/void/"+href;
+function loadFile(href){
+	$("#main")
+		.fadeOut(1000)
+		.load("http://robertberger5.github.io/void/"+href)
+		.fadeIn(1000)
+	;
 };
+
+SC.initialize({
+	client_id: 'Void_Official',
+	redirect_uri: 'robertberger5.github.io/index.html'
+});
+
+SC.connect().then(function() {
+  return SC.get('/me');
+}).then(function(me) {
+  alert('Hello, ' + me.username);
+});
+
+
+$(document).ready(function(){
+	//loadFile("template.html");
+	$("#main").load("http://robertberger5.github.io/void/template.html");
+
+
+	$(document).ready(function() {
+		SC.get('/tracks', { genres: 'Metal' }, function(tracks) {
+			$(tracks).each(function(index, track) {
+				//$('#main').append($('<li></li>').html(track.title + ' - ' + track.genre));
+				console.log(track.title + ' - ' + track.genre);
+			});
+		});
+	});
+
+
+});
