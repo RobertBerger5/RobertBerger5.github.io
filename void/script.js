4f3fbcd8c7d4e94c71b8fc7f575e27cd646fd098
RobertBerger5 <robertberger5@gmail.com>
2016-05-20 18:37:56 -0500

    Update script.js

--- void/script.js
+++ void/script.js
@@ -8,13 +8,7 @@ function loadFile(href){
 
 SC.initialize({
 	client_id: '8a7a1ab91d6a4182bfd718ee80812e00',
-	redirect_uri: 'robertberger5.github.io/void/redirect_uri.html'
-});
-
-SC.connect().then(function() {
-  return SC.get('/me');
-}).then(function(me) {
-  alert('Hello, ' + me.username);
+	redirect_uri: 'http://robertberger5.github.io/void/redirect_uri.html'
 });
 
 
@@ -22,13 +16,16 @@ $(document).ready(function(){
 	//loadFile("template.html");
 	$("#main").load("http://robertberger5.github.io/void/template.html");
 
+	SC.connect().then(function() {
+		return SC.get('/me');
+	}).then(function(me) {
+		alert('Hello, ' + me.username);
+	});
 
-	$(document).ready(function() {
-		SC.get('/tracks', { genres: 'Metal' }, function(tracks) {
-			$(tracks).each(function(index, track) {
-				//$('#main').append($('<li></li>').html(track.title + ' - ' + track.genre));
-				console.log(track.title + ' - ' + track.genre);
-			});
+	SC.get('/tracks', { genres: 'Metal' }, function(tracks) {
+		$(tracks).each(function(index, track) {
+			//$('#main').append($('<li></li>').html(track.title + ' - ' + track.genre));
+			console.log(track.title + ' - ' + track.genre);
 		});
 	});
 
