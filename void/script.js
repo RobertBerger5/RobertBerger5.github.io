399030ad595c047d0601d4732183c32ee1cb15fa
RobertBerger5 <robertberger5@gmail.com>
2016-05-20 18:47:56 -0500

    Update script.js

--- void/script.js
+++ void/script.js
@@ -16,15 +16,28 @@ $(document).ready(function(){
 	//loadFile("template.html");
 	$("#main").load("http://robertberger5.github.io/void/template.html");
 
-	SC.connect().then(function() {
+	SC.initialize({
+		client_id: '8a7a1ab91d6a4182bfd718ee80812e00',
+		redirect_uri: 'http://robertberger5.github.io/void/redirect_uri.html'
+	});
+
+	/*SC.connect().then(function() {
 		return SC.get('/me');
 	}).then(function(me) {
 		alert('Hello, ' + me.username);
-	});
+	});*/
+
+	SC.connect(function(){
+		console.log("e");
+        SC.get('/me', function(me){
+            alert(me.username);
+        });
+    });
 
 	SC.get('/tracks', { genres: 'Metal' }, function(tracks) {
+		console.log("yay?");
 		$(tracks).each(function(index, track) {
-			//$('#main').append($('<li></li>').html(track.title + ' - ' + track.genre));
+			$('#main').append($('<li></li>').html(track.title + ' - ' + track.genre));
 			console.log(track.title + ' - ' + track.genre);
 		});
 	});
