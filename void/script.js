ac09cde08989dddd1ce0c9f0fd111dad6dc21593
RobertBerger5 <robertberger5@gmail.com>
2016-05-20 21:01:49 -0500

    Update script.js

--- void/script.js
+++ void/script.js
@@ -1,44 +1,12 @@
-/*SC.initialize({
-      client_id: "8a7a1ab91d6a4182bfd718ee80812e00",
-      redirect_uri: "http://robertberger5.github.io/void/callback.html",
+SC.initialize({
+  client_id: '8a7a1ab91d6a4182bfd718ee80812e00'
+});
+
+$(document).ready(function() {
+  SC.get('/tracks', { genres: 'shit' }, function(tracks) {
+      console.log("shit");
+    $(tracks).each(function(index, track) {
+      $('#main').append($('<p></p>').html(track.title + ' - ' + track.genre));
+    });
   });
-
-  var userId = 39090345; // user_id of Prutsonic
-
-  SC.get("/tracks", {
-      user_id: userId,
-      limit: 100
-  }, function (tracks) {
-
-
-
-      for (var i = 0; i < tracks.length; i++) {
-          console.log(tracks[i].title);
-      }
-
-  });*/
-
-  SC.initialize({
-      client_id: "8a7a1ab91d6a4182bfd718ee80812e00",
-      redirect_uri: "http://robertberger5.github.io/void/callback.html",
-  });
-
-  //var userId = 39090345; // user_id of Prutsonic
-  var userId=203124528; //user_id of [void] Official
-
-  SC.get("/tracks", {
-      user_id: userId,
-      limit: 100
-  }, function (tracks) {
-
-      var tmp = '';
-
-      for (var i = 0; i < tracks.length; i++) {
-            console.log(tracks[i].title);
-
-          tmp = '<a href="' + tracks[i].permalink_url + '">' + tracks[i].title + ' - ' + tracks[i].duration + '</a>';
-
-          $("<p/>").html(tmp).appendTo("#main");
-      }
-
-  });
+});
