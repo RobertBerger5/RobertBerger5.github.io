e722c1a56698319c90f974cc20e04f392c6c8816
RobertBerger5 <robertberger5@gmail.com>
2016-05-20 20:45:07 -0500

    Update script.js

--- void/script.js
+++ void/script.js
@@ -1,16 +1,7 @@
-SC.initialize({
+/*SC.initialize({
       client_id: "8a7a1ab91d6a4182bfd718ee80812e00",
       redirect_uri: "http://robertberger5.github.io/void/callback.html",
   });
-
-/**
-Once that's done you are all set and ready to call the SoundCloud API. 
-**/
-
-/**
-Call to the SoundCloud API. 
-Retrieves list of tracks, and displays a list with links to the tracks showing 'tracktitle' and 'track duration'
-**/
 
   var userId = 39090345; // user_id of Prutsonic
 
@@ -23,6 +14,30 @@ Retrieves list of tracks, and displays a list with links to the tracks showing '
 
       for (var i = 0; i < tracks.length; i++) {
           console.log(tracks[i].title);
+      }
+
+  });*/
+
+  SC.initialize({
+      client_id: "8a7a1ab91d6a4182bfd718ee80812e00",
+      redirect_uri: "http://robertberger5.github.io/void/callback.html",
+  });
+
+  var userId = 39090345; // user_id of Prutsonic
+
+  SC.get("/tracks", {
+      user_id: userId,
+      limit: 100
+  }, function (tracks) {
+
+      var tmp = '';
+
+      for (var i = 0; i < tracks.length; i++) {
+            console.log(tracks[i].title);
+
+          tmp = '<a href="' + tracks[i].permalink_url + '">' + tracks[i].title + ' - ' + tracks[i].duration + '</a>';
+
+          $("<p/>").html(tmp).appendTo("#main");
       }
 
   });
