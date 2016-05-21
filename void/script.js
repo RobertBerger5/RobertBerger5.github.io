5d9f729148963ca59c582d9aa371a1fbd7e9f9a7
RobertBerger5 <robertberger5@gmail.com>
2016-05-20 21:19:41 -0500

    Update script.js

--- void/script.js
+++ void/script.js
@@ -1,3 +1,11 @@
+function loadFile(href){
+	$("#main")
+		.fadeOut(1000)
+		.load("http://robertberger5.github.io/void/"+href)
+		.fadeIn(1000)
+	;
+};
+
   SC.initialize({
       client_id: "8a7a1ab91d6a4182bfd718ee80812e00",
       redirect_uri: "http://robertberger5.github.io/void/callback.html",
