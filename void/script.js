85f09e5653dd3f48084873014bcbc68c59ce5027
RobertBerger5 <robertberger5@gmail.com>
2016-05-21 12:00:02 -0500

    Update script.js

--- void/script.js
+++ void/script.js
@@ -5,36 +5,21 @@ function loadFile(href){
 		//.fadeIn(1000)
 	;
 };
-loadFile('home.html');
 
-  SC.initialize({
-      client_id: "8ee7b3067929d0440f7065ad8874cad8",
-      redirect_uri: "http://robertberger5.github.io/void/callback.html",
-  });
+SC.initialize({
+	client_id: "8ee7b3067929d0440f7065ad8874cad8",
+	edirect_uri: "http://robertberger5.github.io/void/callback.html",
+});
 
 var userId = 205809940; //OUR FUCKING USER ID YAY
   
 $(document).ready(function() {
-  SC.get("/tracks", {
-      user_id: userId,
-      limit: 100
-  }, function (tracks) {
-
-	SC.oEmbed(tracks[0].permalink_url,document.getElementById('player'))
-      /*var tmp = '';
-
-      for (var i = 0; i < tracks.length; i++) {
-            console.log(tracks[i].title);
-
-          tmp = '<a href="' + tracks[i].permalink_url + '">' + tracks[i].title + ' - ' + tracks[i].duration + '</a>';
-
-          $("<p/>").html(tmp).appendTo("#main");
-      }*/
-
-  });
-  
-  
-    /*SC.get('/tracks/293',function(track){
-        SC.oEmbed(track.permalink_url,document.getElementById('player'))
-    });*/
+	SC.get("/tracks", {
+		user_id: userId
+	}, function (tracks) {
+		for(var a in tracks){
+			console.log(a+": "+tracks[a].title)
+		}
+		SC.oEmbed(tracks[0].permalink_url,document.getElementById('player')) //change the index of the array for different songs
+	});
 });
