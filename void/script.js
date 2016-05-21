734d593cc3314dd590044d1cd2a379d5b4833576
RobertBerger5 <robertberger5@gmail.com>
2016-05-20 20:34:13 -0500

    Update script.js

--- void/script.js
+++ void/script.js
@@ -1,12 +1,28 @@
 SC.initialize({
-  client_id: '8a7a1ab91d6a4182bfd718ee80812e00'
-});
-
-$(document).ready(function() {
-  SC.get('/tracks', { genres: 'shit' }, function(tracks) {
-      console.log("shit");
-    $(tracks).each(function(index, track) {
-      $('#main').append($('<p></p>').html(track.title + ' - ' + track.genre));
-    });
+      client_id: "8a7a1ab91d6a4182bfd718ee80812e00",
+      redirect_uri: "http://example.com/callback.html",
   });
-});
+
+/**
+Once that's done you are all set and ready to call the SoundCloud API. 
+**/
+
+/**
+Call to the SoundCloud API. 
+Retrieves list of tracks, and displays a list with links to the tracks showing 'tracktitle' and 'track duration'
+**/
+
+  var userId = 39090345; // user_id of Prutsonic
+
+  SC.get("/tracks", {
+      user_id: userId,
+      limit: 100
+  }, function (tracks) {
+
+
+
+      for (var i = 0; i < tracks.length; i++) {
+          console.log(tracks[i].title);
+      }
+
+  });
