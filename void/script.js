d5683764af77f66fb43c74c6eaed03fa47fab1a5
RobertBerger5 <robertberger5@gmail.com>
2016-05-21 23:02:23 -0500

    Update script.js

--- void/script.js
+++ void/script.js
@@ -6,9 +6,10 @@ function loadFile(href){
 	;
 };
 
-function millisecondToTime(millisecondz){ //TODO:this
-	var time=millisecondz;
-	return time;
+function millisecondToTime(millis){
+	var minutes = Math.floor(millis / 60000);
+	var seconds = ((millis % 60000) / 1000).toFixed(0);
+	return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
 
 
