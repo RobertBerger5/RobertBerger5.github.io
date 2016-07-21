20f47a6518a3d51b9271c29e1f90384b641c26b6
Robert Berger <robertberger5@gmail.com>
2016-07-21 16:11:32 -0500

    Update curveball.js

--- games/curveball.js
+++ games/curveball.js
@@ -1,10 +1,12 @@
 var canvas=document.querySelector("canvas");
-      canvas.width = $(window).width();//500
-      canvas.height= $(window).height();//500
+      canvas.width = $(window).width();
+      canvas.height= $(window).height();
       var ctx = canvas.getContext("2d");
 
-      var scaleShit=1.5; //highest I can go without it going negative for whatever reason is 1.65 at 400, didn't work tho
-      var threeD=1;
+      var pow=.55;//so things in the distance appear closer, perspective
+      var scaleShit=pow*25;//scale it so they aren't scrunched
+      var threeD=0;//1 or 0
+      var meters=0;//1 or 0
 
       var paused=false;
       function togglePause(){
@@ -16,27 +18,27 @@ var canvas=document.querySelector("canvas");
         }
       }
 
-      var player={
-        x:canvas.width/2,
-        y:canvas.height/2,
-        width:canvas.width/4,
-        height:canvas.height/4,
-        curve:1
-      }
-
       var field={
         width:canvas.width, //x
         height:canvas.height, //y
-        length:300 //z 400
+        length:500 //z
+      }
+
+      var player={
+        x:field.width/2,
+        y:field.height/2,
+        width:field.width/4,
+        height:field.height/4,
+        curve:1 //how much the mouse flicks influence it
       }
 
       var ball={
-        x:field.width/2,
-        y:field.height/2,
-        z:field.length,
+        x:field.width,
+        y:field.height,
+        z:0,
         xV:0,
         yV:0,
-        zV:10,
+        zV:3,
         xA:0,
         yA:0,
         zA:0,
@@ -46,15 +48,23 @@ var canvas=document.querySelector("canvas");
       var enemy={
         x:field.width/2,
         y:field.height/2,
-        width:canvas.width*2,
-        height:canvas.height*2,
-        speed:3
+        width:field.width/4,
+        height:field.height/4,
+        speed:2
       }
 
       var mouseX=0;
       var mouseY=0;
       var lastmouseX=mouseX;
       var lastmouseY=mouseY;
+
+      function point(winner){
+        paused=true;
+        console.log("+1 point to "+winner);
+        setTimeout(function(){
+          paused=false;
+        },1000);
+      }
 
       function getMousePos(canvas,evt){
         var rect=canvas.getBoundingClientRect();
@@ -72,30 +82,25 @@ var canvas=document.querySelector("canvas");
       });
 
       function calcPoint(x,y,dist){
-        if(dist<=0){
-          dist=1;
-        }
-
-        cW=canvas.width-dist*scaleShit;
-        cH=canvas.height-dist*scaleShit;
+        cW=field.width-Math.pow(dist,pow)*scaleShit;
+        cH=field.height-Math.pow(dist,pow)*scaleShit;
+        cX=(field.width-cW)/2;
+        cY=(field.height-cH)/2;
         if(threeD){
-          cX=(canvas.width-cW)/3 +(canvas.width/2 - mouseX)/2;
-          cY=(canvas.height-cH)/3 +(canvas.height/2 - mouseY)/2;
-        }else{
-          cX=(canvas.width-cW)/3;
-          cY=(canvas.height-cH)/3;
+          cX+=(field.width/2 - mouseX)/2;
+          cY+=(field.height/2 - mouseY)/2;
         }
-        var xN=x/canvas.width*cW+cX*(1+x/canvas.width);
-        var yN=y/canvas.height*cH+cY*(1+y/canvas.height);
+        var xN=x/field.width*cW+cX//*(1+x/field.width);
+        var yN=y/field.height*cH+cY//*(1+y/field.height);
         return[xN,yN,cX,cY,cW,cH];//returns x, y, canvas x, canvas y, canvas width, canvas height
       }
 
-      function hitBall(person){
+      function hitBall(person){ //3D mode complicates this...should I make it a function of mouseX and mouseY if threeD is enabled?
         if(
-          ball.x-ball.radius<=person.x &&
-          ball.x+ball.radius>=person.x &&
-          ball.y-ball.radius<=person.y &&
-          ball.y+ball.radius>=person.y
+          ball.x-ball.radius<=person.x+person.width/2 &&
+          ball.x+ball.radius>=person.x-person.width/2 &&
+          ball.y-ball.radius<=person.y+person.height/2 &&
+          ball.y+ball.radius>=person.y-person.height/2
         ){
           return true;
         }else{
@@ -123,17 +128,26 @@ var canvas=document.querySelector("canvas");
           bounced=true;
         }
 
-        if(ball.z<=0 && ball.zV<0 && hitBall(player)){
+        if(ball.z<=0 && ball.zV<0){
           ball.zV=ball.zV*-1;
           ball.zA=ball.zA*-1;
-          bounced=true;
+          if(hitBall(player)){
+            bounced=true;
+            console.log("yay");
+          }else{
+            point("enemy");
+          }
 
           ball.xV+=(mouseX-lastmouseX)*player.curve; //used to have it control acceleration but that was too crazy
           ball.yV+=(mouseY-lastmouseY)*player.curve;
-        }else if(ball.z>=field.length && ball.zV>0 /*&& hitBall(enemy)*/){
+        }else if(ball.z>=field.length && ball.zV>0){
           ball.zV=ball.zV*-1;
           ball.zA=ball.zA*-1;
-          bounced=true;
+          if(hitBall(enemy)){
+            bounced=true;
+          }else{
+            point("player");
+          }
         }
 
 
@@ -153,8 +167,8 @@ var canvas=document.querySelector("canvas");
       }
 
       function update(){
-        player.x=mouseX;
-        player.y=mouseY;
+        player.x=mouseX; //makes it so you don't always hit as you see it in 3D mode
+        player.y=mouseY; //ditto
         updateBall();
 
         if(enemy.x<ball.x){
@@ -172,75 +186,57 @@ var canvas=document.querySelector("canvas");
       function render(){
         //black background
         ctx.fillStyle="#000";
-        ctx.fillRect(0,0,canvas.width,canvas.height);
+        ctx.fillRect(0,0,field.width,field.height);
         //enemy
         var enem=calcPoint(enemy.x,enemy.y,field.length);
         ctx.fillStyle="#f00";
-        var drawx=enemy.x/canvas.width*enem[4]+enem[2]*1.5;
-        var drawy=enemy.y/canvas.height*enem[5]+enem[3]*1.5;
-        var draww=enemy.width/canvas.width*enem[4];
-        var drawh=enemy.height/canvas.height*enem[5];
-        //ctx.fillRect(drawx,drawy,draww,drawh); //not drawn to perspective scale
-
+        var enemWidth=enemy.width*enem[4]/field.width;
+        var enemHeight=enemy.height*enem[5]/field.height;
+        ctx.fillRect(enem[0]-enemWidth/2,enem[1]-enemHeight/2,enemWidth,enemHeight); //not drawn to perspective scale
         //lines in the field
         ctx.strokeStyle="#0f0";
-        for(var i=0;i<=field.length;i+=field.length/5){
-          var strokeString="rgb(0,"+(325-Math.floor(i/(field.length/255)))+",0)";
+        for(var i=0;i<=field.length;i+=100){
+          var strokeString="rgb(0,"+(255-Math.floor(i/(field.length/200)))+",0)";
           ctx.strokeStyle=strokeString; //fade farther lines, helps 3D look, diagonal lines look like shit
           //make a gradiant on the lines? maybe.
           var rect=calcPoint(0,0,i);
           ctx.beginPath();
-          ctx.strokeRect(rect[2]*1.5,rect[3]*1.5,rect[4],rect[5]);//used to add x to widths
+          ctx.strokeRect(rect[2],rect[3],rect[4],rect[5]);//used to add x to widths
           ctx.stroke();
         }
         var close=calcPoint(0,0,0);
         var far=calcPoint(0,0,field.length);
-        var numthing=1.5;
         ctx.strokeStyle="#0f0";
         ctx.beginPath();
-        ctx.moveTo(close[2]*numthing,close[3]*numthing);
-        ctx.lineTo(far[2]*numthing,far[3]*numthing);
-        ctx.moveTo(close[2]*numthing+close[4],close[3]*numthing);
-        ctx.lineTo(far[2]*numthing+far[4],far[3]*numthing);
-        ctx.moveTo(close[2]*numthing,close[3]*numthing+close[5]);
-        ctx.lineTo(far[2]*numthing,far[3]*numthing+far[5]);
-        ctx.moveTo(close[2]*numthing+close[4],close[3]*numthing+close[5]);
-        ctx.lineTo(far[2]*numthing+far[4],far[3]*numthing+far[5]);
+        ctx.moveTo(close[2],close[3]);
+        ctx.lineTo(far[2],far[3]);
+        ctx.moveTo(close[2]+close[4],close[3]);
+        ctx.lineTo(far[2]+far[4],far[3]);
+        ctx.moveTo(close[2],close[3]+close[5]);
+        ctx.lineTo(far[2],far[3]+far[5]);
+        ctx.moveTo(close[2]+close[4],close[3]+close[5]);
+        ctx.lineTo(far[2]+far[4],far[3]+far[5]);
         ctx.stroke();
-        /*ctx.beginPath();
-        ctx.moveTo(0,0);
-        ctx.lineTo(lastC.x,lastC.y);
-        ctx.moveTo(field.width,0);
-        ctx.lineTo(lastC.x*2+lastC.w,lastC.y);
-        ctx.moveTo(0,field.height);
-        ctx.lineTo(lastC.x,lastC.y*2+lastC.h);
-        ctx.moveTo(field.width,field.height);
-        ctx.lineTo(lastC.x*2+lastC.w,lastC.y*2+lastC.h);
-        ctx.stroke();*/
 
         var ballShit=calcPoint(ball.x,ball.y,ball.z); //returns new x, new y, canvas x, canvas y, canvas width, canvas height
         //rectangle showing ball z position
         ctx.strokeStyle="#fff";
-        ctx.strokeRect(ballShit[2]*1.5,ballShit[3]*1.5,ballShit[4],ballShit[5]);//used to add x and y to w and h
+        ctx.strokeRect(ballShit[2],ballShit[3],ballShit[4],ballShit[5]);//used to add x and y to w and h
         //ball
         ctx.fillStyle="#0f0";
         ctx.beginPath();
         var adjust=scaleShit*100;
-        var rad=ball.radius*adjust/(ball.z+adjust);
-        if(rad>=0){
-          ctx.arc(ballShit[0],ballShit[1],rad,0,2*Math.PI); //fine at both ends but not the middle, wtf
-        }else{
-          paused=true;
-          console.log("YOU LOSE");
-        }
+        //var rad=ball.radius*adjust/(ball.z+adjust);
+        var rad=ball.radius*(ballShit[4]/field.width+ballShit[5]/field.height)/2;
+        ctx.arc(ballShit[0],ballShit[1],rad,0,2*Math.PI);//rad<0?
         ctx.closePath();
         ctx.fill();
         //ghost ball
         ctx.fillStyle="rgba(0,255,0,.5";
         ctx.beginPath();
-        ctx.arc(ball.x,ball.y,ball.radius,0,2*Math.PI); //fine at both ends but not the middle, wtf
+        ctx.arc(ball.x,ball.y,ball.radius,0,2*Math.PI);
         ctx.closePath();
-        //ctx.fill();
+        ctx.fill();
         //ghost enemy
         ctx.fillStyle="rgba(255,0,0,.5";
         //ctx.fillRect(enemy.x-enemy.width/2,enemy.y-enemy.height/2,enemy.width,enemy.height);
@@ -248,11 +244,22 @@ var canvas=document.querySelector("canvas");
         //player
         ctx.fillStyle="rgba(0,0,255,.5)";
         ctx.fillRect(player.x-player.width/2,player.y-player.height/2,player.width,player.height);
-        //ball distance meter
-        ctx.fillStyle="#f00";
-        ctx.fillRect(0,0,canvas.width,10);
-        ctx.fillStyle="#0f0";
-        ctx.fillRect(0,0,canvas.width*(ball.z/field.length),10);
+
+        //ball distance meters
+        if(meters){
+          ctx.fillStyle="#f00";
+          ctx.fillRect(0,0,canvas.width,10);
+          ctx.fillStyle="#0f0";
+          ctx.fillRect(0,0,canvas.width*(ball.z/field.length),10);
+          ctx.fillStyle="#f00";
+          ctx.fillRect(ball.radius,canvas.height-10,canvas.width-ball.radius*2,10);
+          ctx.fillStyle="#0f0";
+          ctx.fillRect(ball.radius,canvas.height-10,canvas.width*(ball.x/field.width)-ball.radius,10);
+          ctx.fillStyle="#f00"
+          ctx.fillRect(0,ball.radius,10,canvas.height-ball.radius*2);
+          ctx.fillStyle="#0f0";
+          ctx.fillRect(0,ball.radius,10,canvas.height*(ball.y/field.height)-ball.radius);
+        }
       }
 
       setInterval(function(){
@@ -261,4 +268,4 @@ var canvas=document.querySelector("canvas");
           render();
           //sendShit();
         }
-      },50);
+      },10);
