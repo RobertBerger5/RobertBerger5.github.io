6536ad0a72f32f2a4b326bc1a791f87077d4a4ac
Rob Berger <robertberger5@gmail.com>
2019-01-03 10:25:01 -0600

    minor changes (I think)

---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+    //TODO: have it calculate the tallest item in the carousel and make all carousel items that height
+});
