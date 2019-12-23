8d03dd9dacb578eb81643db1d5c43d0b01166b03
Rob Berger <robertberger5@gmail.com>
2019-12-23 13:06:20 -0600

    updates to frontend, 12/23/19

--- rate.php
+++ rate.php
@@ -8,7 +8,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="stylesheet.css">
-  <link rel="icon" type="image/png" href="images/slayericon.ico" />
+  <link rel="icon" type="image/png" href="media/slayericon.ico" />
 </head>
 <body>
   <script>
